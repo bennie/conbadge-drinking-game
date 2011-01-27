aa6ddef407dfbf8113be2fe27e8b18986304b6e4
Phillip Pollard <phil@crescendo.net>
2011-01-27 00:36:57 +0000

    Making it standalone from the old AC code.

--- drinking.pl
+++ drinking.pl
@@ -1,56 +1,58 @@
-#!/usr/bin/perl -I/home/httpd/code/lib
+#!/usr/bin/perl
 
-use Anthrocon::SQL;
-use Anthrocon::Time;
 use strict;
-
-my $as = new Anthrocon::SQL;
-my $at = new Anthrocon::Time;
-
-my $conyear = $at->con_year();
 
 my %animals;
 
+$animals{aquatic}   = [ qr/fish/i, qr/orca/i ];
 $animals{badger}    = [ qr/badger/i ];
 $animals{bat}       = [ qr/bat/i ];
-$animals{bear}      = [ qr/bear/i ];
+$animals{bear}      = [ qr/bear/i, qr/panda/i ];
 $animals{bird}      = [ qr/bird/i, qr/crow/i, qr/raven/i ];
+$animals{buffalo}   = [ qr/buffalo/i ];
 $animals{cat}       = [ qr/[ckq]at/i, qr/kitten/i, qr/kitty/i, qr/tabbie/i ];
 $animals{cheetah}   = [ qr/cheetah/i ];
 $animals{chipmunk}  = [ qr/chipmunk/i ];
 $animals{cougar}    = [ qr/couga?r/i ];
 $animals{coyote}    = [ qr/([ck]o)?yote/i ];
 $animals{deer}      = [ qr/deer/i ];
-$animals{dog}       = [ qr/dog/i, qr/husky/i, qr/pup(py)?/i, qr/hound/i, qr/canine/i ];
-$animals{dragon}    = [ qr/dragon/i ];
+$animals{dog}       = [ qr/can[iu]{0,2}s/i, qr/collie/i, qr/dog/i, qr/husky/i, qr/pup(py)?/i, qr/hound/i, qr/canine/i, qr/dingo/i ];
+$animals{dragon}    = [ qr/dragon/i, qr/hydra/i ];
 $animals{ferret}    = [ qr/ferret/i ];
-$animals{fox}       = [ qr/fox/i, qr/fennec/i, qr/kitsune/i, qr/vulpin(e)?/i, qr/vulpe(s)?/i ];
+$animals{fox}       = [ qr/fox/i, qr/fennec/i, qr/kitsune/i, qr/vixen/i, qr/vulpin(e)?/i, qr/vulpe(s)?/i ];
 $animals{gazelle}   = [ qr/gazelle/i ];
 $animals{goat}      = [ qr/goat/i ];
-$animals{gryphon}   = [ qr/gryphon/i ];
+$animals{gryphon}   = [ qr/gryphon/i, qr/griff[io]n/i ];
 $animals{horse}     = [ qr/horse/i, qr/pon[iy]/i ];
 $animals{hyena}     = [ qr/hyena/i ];
+$animals{jaguar}    = [ qr/jaguar/i ];
 $animals{lion}      = [ qr/lion/i ];
+$animals{lizard}    = [ qr/lizard/i ];
 $animals{lynx}      = [ qr/lynx/i ];
 $animals{mouse}     = [ qr/mous(i)?e/i ];
 $animals{otter}     = [ qr/otter/i ];
 $animals{panther}   = [ qr/panther/i ];
-$animals{rabbit}    = [ qr/rabbit/i, qr/bunny/i, qr/bunnie/i, qr/bunn\b/i ];
+$animals{rabbit}    = [ qr/rabbit/i, qr/bunny/i, qr/bunnie/i, qr/bunn\b/i, qr/lapine/i ];
 $animals{racoon}    = [ qr/(ra)?coon/i ];
 $animals{rat}       = [ qr/rat/i ];
 $animals{roo}       = [ qr/kangaroo/i, qr/roo\b/i ];
+$animals{sheep}     = [ qr/sheep/i, qr/lamb/i ];
 $animals{skunk}     = [ qr/skunk(ie)?/i ];
 $animals{snake}     = [ qr/snake/i, qr/python/i ];
 $animals{squirrel}  = [ qr/squirrel/i ];
 $animals{tiger}     = [ qr/t[iy]g(re|e?r)/i, qr/tora/i ];
 $animals{unicorn}   = [ qr/unicorn/i ];
-$animals{weasel}    = [ qr/weasel/i ];
-$animals{wolf}      = [ qr/w[ou]l[fv](i?e)?([ei]n)?(e?y)?/i, qr/lupin(e)?/i ];
+$animals{weasel}    = [ qr/weasel/i, qr/mongoose/i ];
+$animals{wolf}      = [ qr/w[ou]l[fv](i?e)?([ei]n)?(e?y)?/i, qr/lupin(e)?/i, qr/whuff/ ];
 $animals{woodchuck} = [ qr/woodchuck/i ];
 $animals{zebra}     = [ qr/zebra/i ];
 
+### Colors & metals
+
 my %colors;
-map {$colors{$_} = [ qr/$_/i ];} qw/black blue brown green red white yellow/;
+map {$colors{$_} = [ qr/$_/i ];} qw/black blue brown emerald green grey red white yellow/;
+
+$colors{purple} = [ qr/pu?rple?/i ];
 
 my %metals;
 map {$metals{$_} = [ qr/$_/i ];} qw/brass cobalt copper gold iron metal silver/;
@@ -59,17 +61,22 @@ my %seasons;
 map {$seasons{$_} = [ qr/$_/i ];} qw/spring summer fall winter/;
 
 my %elements;
-map {$elements{$_} = [ qr/$_/i ];} qw/earth wind fire water/;
+map {$elements{$_} = [ qr/$_/i ];} qw/earth wind fire flame water/;
 
 my %celestials;
 
-$celestials{cloud}   = [ qr/cloud/i ];
+$celestials{angel}   = [ qr/angel/i ];
+$celestials{anubis}  = [ qr/anubis/i ];
+$celestials{demon}   = [ qr/demon/i ];
+$celestials{god}     = [ qr/god/i ];
+
 $celestials{moon}    = [ qr/moon/i ];
-$celestials{thunger} = [ qr/thunder/i ];
+$celestials{thunder} = [ qr/thunder/i ];
 $celestials{sky}     = [ qr/\bsky/i ];
 $celestials{shadow}  = [ qr/shadow/i ];
-$celestials{shadow}  = [ qr/snow/i ];
+$celestials{snow}    = [ qr/snow/i ];
 $celestials{star}    = [ qr/star/i ];
+$celestials{storm}   = [ qr/storm/i ];
 $celestials{sun}     = [ qr/\bsun/i ];
 
 ## Assemble the tests
@@ -85,11 +92,10 @@ $tests{celestial} = \%celestials;
 
 # Load unique badge names
 
-my @names = $as->query_column('select badge_name from reg where con_year=?',$conyear);
 my %unique;
-map {$unique{$_}++} @names;
+map { chomp; $unique{$_}++ } <STDIN>;
 
-@names = sort keys %unique;
+my @names = sort { lc($a) cmp lc($b) } keys %unique;
 %unique = ();
 
 ### Run the tests
@@ -112,7 +118,8 @@ for my $name (@names) {
       }
     }
   }
-  #if ( not defined $scores{$name} ) { print "No Score: $name\n"; }
+  if ( not defined $scores{$name} ) { print "No Score: $name\n"; }
+        #print "$name : $scores{$name}\n";
 }
 
 print "$drinks drinks.\n";
@@ -141,5 +148,18 @@ for my $name ( sort { lc($a) cmp lc($b) } keys %scores ) {
 
   for my $parse ( keys %{$parses{$name}} ) {
     print "\t -> $parse\n";
+  }
+}
+
+$best--;
+
+print "The runners-up are $best. The following names scored this:\n";
+
+for my $name ( sort { lc($a) cmp lc($b) } keys %scores ) {
+  next unless $scores{$name} == $best;
+  print "\t$name\n";
+
+  for my $parse ( keys %{$parses{$name}} ) {
+    #print "\t -> $parse\n";
   }
 }
